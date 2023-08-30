e12fc8e3eeea0902d3d160c5166bc38278142822
elliotkiang <142453988+elliotkiang@users.noreply.github.com>
2023-08-30 15:26:52 -0700

    Updating demos/java-canvas/MyPicture.java

--- demos/java-canvas/MyPicture.java
+++ demos/java-canvas/MyPicture.java
@@ -6,9 +6,9 @@ public class MyPicture implements Picture {
     for (var x = 0; x < c.width(); x += 20) {
       c.drawLine(x, 0, x, c.height(), "rgba(0, 128, 192, 0.5)", 2);
     }
-    c.drawLine(0, 0, c.width(), c.height(), "white", 10);
-    c.drawLine(0, c.height(), c.width(), 0, "red", 10);
-    c.drawFilledCircle(c.width() / 1, c.height() / 1, 100.0, "blue");
+    c.drawLine(0, 0, c.width(), c.height(), "white", 100);
+    c.drawLine(0, c.height(), c.width(), 0, "red", 100);
+    c.drawFilledCircle(c.width() / 2, c.height() / 2, 100.0, "blue");
     c.drawFilledRect(20, 20, c.width() - 40, c.height() - 40, "#ff00ff20");
   }
 }
