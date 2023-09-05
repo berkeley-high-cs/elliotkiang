7115fab25bc2cd8b984572bedf87f0addc911d3e
elliotkiang <142453988+elliotkiang@users.noreply.github.com>
2023-09-05 15:09:35 -0700

    Updating assignments/geometry/Geometry.java

--- assignments/geometry/Geometry.java
+++ assignments/geometry/Geometry.java
@@ -5,11 +5,15 @@
 
 public class Geometry {
 
+  public double midpoint(double one, double two){
+    return (one+two)/2
+  }
   public double perimeterOfSquare(double side) {
     return side * 4;
   }
   public double perimeterOfRectangle(double width, double height){
     return 2*(height+width);
   }
+
   // Add more methods here ...
 }
