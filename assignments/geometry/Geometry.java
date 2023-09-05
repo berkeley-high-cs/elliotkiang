3d10449e6ea2cf2bb9fe3ae940c2a45ff699802d
elliotkiang <142453988+elliotkiang@users.noreply.github.com>
2023-09-05 15:02:29 -0700

    Updating assignments/geometry/Geometry.java

--- assignments/geometry/Geometry.java
+++ assignments/geometry/Geometry.java
@@ -11,8 +11,8 @@ public class Geometry {
   public double perimeterOfRectangle(double length, double width){
     length=5;
     width=4;
-    return length*width;
     System.out.println(length*width);
+    return length*width;
 
   }
   // Add more methods here ...
