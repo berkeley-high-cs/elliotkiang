87ee7ecf9f297e02e0ce4185d4681de0da6f9ac4
elliotkiang <142453988+elliotkiang@users.noreply.github.com>
2023-09-05 14:45:54 -0700

    Creating assignments/geometry/Geometry.java

---
+++ assignments/geometry/Geometry.java
@@ -0,0 +1,13 @@
+/*
+ * Just add test methods like the one shown below. (Oh, and maybe fix the bug in
+ * subtract.) All numbers are double.
+ */
+
+public class Geometry {
+
+  public double perimeterOfSquare(double side) {
+    return side * 4;
+  }
+
+  // Add more methods here ...
+}
