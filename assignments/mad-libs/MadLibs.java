64e09b2265e3ceb88eb784a6351a85f137eb2ede
elliotkiang <142453988+elliotkiang@users.noreply.github.com>
2023-09-11 15:23:04 -0700

    Updating assignments/mad-libs/MadLibs.java

--- assignments/mad-libs/MadLibs.java
+++ assignments/mad-libs/MadLibs.java
@@ -12,9 +12,6 @@ public class MadLibs {
   
   private String[] nouns = {"bucket", "book", "magazine", "detached arm", "classroom", "computer"};
 
-  // This method chooses a random element from the array passed as its argument.
-  // For instance given the definition of names above, chooseFrom(names) will
-  // return one of "Wilma", "Betty", "Fred", or "Barney" each time it is called.
   public String cF(String[] choices) {
     return choices[(int) (Math.random() * choices.length)];
   }
@@ -25,9 +22,6 @@ public class MadLibs {
     return verb + "ed";
   }
 
-  // This is a sample method that produces a sentence filling in some values
-  // just like when you fill in the blanks in a MadLib. You should write several
-  // more of these to make a full MadLib.
   public String sentenceOne(String name, String adjective, String verb) {
     return "Hello " + name + ", you're looking " + adjective + " as you " + verb +" today.";
   }
