694093370a789ea828874b513c5af620e8bd7695
elliotkiang <142453988+elliotkiang@users.noreply.github.com>
2023-09-13 14:43:21 -0700

    Updating assignments/simple-graphics/SimpleGraphics.java

--- assignments/simple-graphics/SimpleGraphics.java
+++ assignments/simple-graphics/SimpleGraphics.java
@@ -12,40 +12,7 @@ public class SimpleGraphics implements ImageGenerator {
   // (You'll need to pass the Graphics object g to those methods.)
   public void draw(Graphics g, int width, int height) {
     g.setColor(Color.GREEN);
-    g.fillRect(0, 0, 25, 500);
-    g.setColor(Color.RED);
-    g.fillRect(50, 0, 25, 500);
-    g.setColor(Color.YELLOW);
-    g.fillRect(100, 0, 25, 500);
-    g.setColor(Color.PINK);
-    g.fillRect(150,0,25,500);
-    g.setColor(Color.YELLOW);
-    g.fillRect(200, 0, 25, 500);
-    g.setColor(Color.RED);
-    g.fillRect(250, 0, 25, 500);
-    g.setColor(Color.GREEN);
-    g.fillRect(300, 0, 25, 500);
-    g.setColor(Color.RED);
-    g.fillRect(350, 0, 25, 500);
-    g.setColor(Color.YELLOW);
-    g.fillRect(400, 0, 25, 500);
-    g.setColor(Color.PINK);
-    g.fillRect(450,0,25,500);
-    g.setColor(Color.YELLOW);
-    g.fillRect(500, 0, 25, 500);
-    g.setColor(Color.RED);
-    g.fillRect(550, 0, 25, 500);
-    g.setColor(Color.GREEN);
-    g.fillRect(600, 0, 25, 500);
-    g.setColor(Color.RED);
-    g.fillRect(650, 0, 25, 500);
-    g.setColor(Color.YELLOW);
-    g.fillRect(700, 0, 25, 500);
-    g.setColor(Color.PINK);
-    g.fillRect(750,0,25,500);
-    g.setColor(Color.YELLOW);
-    g.fillRect(800,0,25,500);
-    System.out.println ("I call this *the lines*");
+    g.fillOval(width,height/2, 10, 10);
   }
 
 }
