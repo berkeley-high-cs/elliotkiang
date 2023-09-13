ba764a56f84776e383c8ad8b6aa9fa4134a7ef8a
elliotkiang <142453988+elliotkiang@users.noreply.github.com>
2023-09-13 14:44:46 -0700

    Updating assignments/simple-graphics/SimpleGraphics.java

--- assignments/simple-graphics/SimpleGraphics.java
+++ assignments/simple-graphics/SimpleGraphics.java
@@ -12,9 +12,9 @@ public class SimpleGraphics implements ImageGenerator {
   // (You'll need to pass the Graphics object g to those methods.)
   public void draw(Graphics g, int width, int height) {
     g.setColor(Color.GREEN);
-    g.fillOval(width/4,height/2, 100, 100);
+    g.fillOval(width/4,height/4, 100, 100);
     g.setColor(Color.RED);
-    g.fillOval(width/4*2,height/2, 100, 100);
+    g.fillOval(width/4*2,height/4, 100, 100);
   }
 
 }
