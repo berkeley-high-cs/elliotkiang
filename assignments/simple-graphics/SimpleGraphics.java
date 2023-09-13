0326617f1c50f6f2c9212113f09c72a8f107c5a1
elliotkiang <142453988+elliotkiang@users.noreply.github.com>
2023-09-13 14:45:06 -0700

    Updating assignments/simple-graphics/SimpleGraphics.java

--- assignments/simple-graphics/SimpleGraphics.java
+++ assignments/simple-graphics/SimpleGraphics.java
@@ -14,7 +14,7 @@ public class SimpleGraphics implements ImageGenerator {
     g.setColor(Color.GREEN);
     g.fillOval(width/4,height/4, 100, 100);
     g.setColor(Color.RED);
-    g.fillOval(width/4*2,height/4, 100, 100);
+    g.fillOval(2*(width/4),height/4, 100, 100);
   }
 
 }
