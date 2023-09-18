c0c5279f5cfceed10d9ea9f46c8a296a1944f226
elliotkiang <142453988+elliotkiang@users.noreply.github.com>
2023-09-18 14:55:41 -0700

    Creating assessments/csa-unit-sqrt-2/AssessmentSqrt2.java

---
+++ assessments/csa-unit-sqrt-2/AssessmentSqrt2.java
@@ -0,0 +1,8 @@
+public class AssessmentSqrt2 {
+
+  // You will need this constant for one problem.
+  public static final double C = 299792458.0;
+
+  // Write methods here. They should be public but not static.
+
+}
