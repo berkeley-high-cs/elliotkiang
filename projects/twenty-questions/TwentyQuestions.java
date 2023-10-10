2b15cb3b3b70a7fe9c93f5cee928c31a5cc56480
elliotkiang <142453988+elliotkiang@users.noreply.github.com>
2023-10-10 21:46:01 +0000

    Twenty Questions Done?

--- projects/twenty-questions/TwentyQuestions.java
+++ projects/twenty-questions/TwentyQuestions.java
@@ -11,12 +11,112 @@ public class TwentyQuestions {
   private Scanner input = new Scanner(System.in);
 
   public void play() {
-    System.out.print("Is it an elephant? ");
+    String win = "cower before me, puny mortal";
+    String lose = "aw shucks";
+    System.out.print("Is it alive? ");
     String answer = input.nextLine();
     if (answer.toLowerCase().startsWith("y")) {
-      System.out.println("Yay! I win.");
+      System.out.println("Does it live in the ocean?");
+      String answer1 = input.nextLine();
+      if (answer1.toLowerCase().startsWith("y"))
+      {
+        System.out.println("Does it have fins?");
+        String answer2 = input.nextLine();
+        if (answer2.toLowerCase().startsWith("y")){
+          System.out.println("Is it a shark?");
+          String answerF = input.nextLine();
+        if (answerF.toLowerCase().startsWith("y")){
+          System.out.println(win);
+        } else
+        {
+          System.out.println(lose);
+        }
+          
+        } else
+      {
+        System.out.println("Is it a sea slug?");
+        String answerF = input.nextLine();
+        if (answerF.toLowerCase().startsWith("y")){
+          System.out.println(win);
+        } else
+        {
+          System.out.println(lose);
+        }
+      }
+      } else
+      {
+        System.out.println("Does it have fur?");
+        String answer3 = input.nextLine();
+        if (answer3.toLowerCase().startsWith("y")){
+          System.out.println("Is it a sloth?");
+          String answerF = input.nextLine();
+        if (answerF.toLowerCase().startsWith("y")){
+          System.out.println(win);
+        } else
+        {
+          System.out.println(lose);
+        }
+        }
+        else {
+          System.out.println("Is it a person?");
+          String answerF = input.nextLine();
+        if (answerF.toLowerCase().startsWith("y")){
+          System.out.println(win);
+        } else
+        {
+          System.out.println(lose);
+        }
+        }
+      }
     } else {
-      System.out.println("Dang it.");
+      System.out.println("Is it gray?");
+      String answer1 = input.nextLine();
+      if (answer1.toLowerCase().startsWith("y")){
+        System.out.println("Is it hard?");
+        String answer2 = input.nextLine();
+        if (answer2.toLowerCase().startsWith("y")){
+          System.out.println("Is it a rock?");
+          String answerF = input.nextLine();
+        if (answerF.toLowerCase().startsWith("y")){
+          System.out.println(win);
+        } else
+        {
+          System.out.println(lose);
+        }
+        } else {
+          System.out.println("Is it ashes?");
+          String answerF = input.nextLine();
+        if (answerF.toLowerCase().startsWith("y")){
+          System.out.println(win);
+        } else
+        {
+          System.out.println(lose);
+        }
+        }
+      } else {
+        System.out.println("Is it in nature?");
+        String answer3 = input.nextLine();
+        if (answer3.toLowerCase().startsWith("y")){
+          System.out.println("Is it dirt?");
+          String answerF = input.nextLine();
+        if (answerF.toLowerCase().startsWith("y")){
+          System.out.println(win);
+        } else
+        {
+          System.out.println(lose);
+        }
+        }
+        else {
+          System.out.println("Is it glass?");
+          String answerF = input.nextLine();
+        if (answerF.toLowerCase().startsWith("y")){
+          System.out.println(win);
+        } else
+        {
+          System.out.println(lose);
+        }
+        }
+      }
     }
   }
 
