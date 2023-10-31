49a70e166e86e94ceb8eb7a4a44a62772f844948
elliotkiang <142453988+elliotkiang@users.noreply.github.com>
2023-10-31 14:37:29 -0700

    Creating assignments/primes/Primes.java

---
+++ assignments/primes/Primes.java
@@ -0,0 +1,12 @@
+/*
+ * A number is prime if it is not divisible by any number other than 1 and
+ * itself. 1 is not prime. Thus you can test whether a number greater than 1 is
+ * prime by checking whether it is divisible by any smaller number greater than
+ * one.
+ */
+
+public class Primes {
+
+
+
+}
