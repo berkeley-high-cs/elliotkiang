f1763f8089cd0a7746fddfd9855ef33be95a0782
elliotkiang <142453988+elliotkiang@users.noreply.github.com>
2023-11-13 14:58:43 -0800

    Updating assignments/sieve/Sieve.java

--- assignments/sieve/Sieve.java
+++ assignments/sieve/Sieve.java
@@ -6,4 +6,13 @@ public class Sieve {
     }
     return ray;
   }
+  public int countNonZeros(int[] a){
+    int count=0;
+    for(int i=0;i<a.length;i++){
+      if(a[i]!=0){
+        count++;
+      }
+    }
+    return count;
+  }
 }
