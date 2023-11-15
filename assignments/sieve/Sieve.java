d624b315b02725e817d4a1fb95f43f5db7cf999f
elliotkiang <142453988+elliotkiang@users.noreply.github.com>
2023-11-15 15:06:50 -0800

    Updating assignments/sieve/Sieve.java

--- assignments/sieve/Sieve.java
+++ assignments/sieve/Sieve.java
@@ -25,7 +25,7 @@ public class Sieve {
   }
   public int[] nonZeros(int[] a){
     int[]b = new int[countNonZeros(a)];
-    int pos =0;
+    int pos = 0;
     for(int i=0;i<a.length;i++){
       if(a[i] != 0){
         b[pos]=a[i];
