fd0b212e354d2728556656c3b2a8836687262f5b
elliotkiang <142453988+elliotkiang@users.noreply.github.com>
2023-11-28 14:47:29 -0800

    Updating assignments/point/Point.java

--- assignments/point/Point.java
+++ assignments/point/Point.java
@@ -4,7 +4,7 @@ public class Point{
   }
   private double x = 0;
   private double y = 0;
-  public void constructor(double numX, double numY){
+  public constructor(double numX, double numY){
     x = numX;
     y = numY;
   }
