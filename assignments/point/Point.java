4ea7ab4ece4bb9692c1497d527ba6e86dbe81e45
elliotkiang <142453988+elliotkiang@users.noreply.github.com>
2023-11-28 14:56:19 -0800

    Updating assignments/point/Point.java

--- assignments/point/Point.java
+++ assignments/point/Point.java
@@ -7,7 +7,7 @@ public class Point{
   }
   public static void main (String args[]){
     System.out.println("hello, world!");
-    Point ah = new Point(10,11);
+    Point ah = new Point(10.0,11.0);
     System.out.println(ah);
   }
 }
