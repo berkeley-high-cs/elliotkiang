6bb3f38fb29adc751b787a9ef934fca5eadda1e0
elliotkiang <142453988+elliotkiang@users.noreply.github.com>
2023-11-29 14:43:04 -0800

    Updating assignments/point/Point.java

--- assignments/point/Point.java
+++ assignments/point/Point.java
@@ -5,9 +5,13 @@ public class Point{
     x = numX;
     y = numY;
   }
+  public double distanceFromOrigin(){
+    return(Math.hypot(x,y));
+  }
   public static void main (String args[]){
     System.out.println("hello, world!");
     Point ah = new Point(10.0,11.0);
+    ah.distanceFromOrigin();
     System.out.println(ah);
   }
 }
