4c3ae859476a326023743bce36febfecda4c055e
elliotkiang <142453988+elliotkiang@users.noreply.github.com>
2023-11-30 15:03:07 -0800

    Updating assessments/flag/Flag.java

--- assessments/flag/Flag.java
+++ assessments/flag/Flag.java
@@ -36,9 +36,9 @@ public class Flag implements ImageGenerator {
     }
     g.setColor(Color.BLUE);
     g.fillRect(0,0,(int)(width*0.4+0.5),7*height/13);
-    //for(int i=1;i<=50;i++){
-    //drawStar(g, WHITE, width/2, height/2, 100);
-    //}
+    for(int i=1;i<=50;i++){
+    drawStar(g, WHITE, width/2, height/2, 100);
+    }
   }
 
   ////////////////////////////////////////////////////////////////////////
