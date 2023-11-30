1c98ff182506370f4a40d2ed526d945b7c155b82
elliotkiang <142453988+elliotkiang@users.noreply.github.com>
2023-11-30 14:43:04 -0800

    Updating assessments/flag/Flag.java

--- assessments/flag/Flag.java
+++ assessments/flag/Flag.java
@@ -24,7 +24,9 @@ public class Flag implements ImageGenerator {
     // next line but to get you started, here's an example of calling the
     // provided drawStar method to draw a star, in this case a red star with a
     // diameter of 100 at the center of the canvas.
+    for(int i=0;i<50;i++){
     drawStar(g, WHITE, width / 2, height / 2, 100);
+    }
   }
 
   ////////////////////////////////////////////////////////////////////////
