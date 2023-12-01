564c36f2ffe76182ac45963e70645fc66ffe3876
elliotkiang <142453988+elliotkiang@users.noreply.github.com>
2023-12-01 14:51:27 -0800

    Updating assignments/point/Point.java

--- assignments/point/Point.java
+++ assignments/point/Point.java
@@ -25,8 +25,9 @@ public class Point{
   public static void main (String args[]){
     System.out.println("hello, world!");
     Point ah = new Point(300.0,400.0);
+    Point origin = new Point(0.0,0.0);
     System.out.println("p:" + ah);
-    System.out.println("midoint:" + ah.midwayTo());
+    System.out.println("midoint:" + ah.midwayTo(origin));
     System.out.println("distance from origin:" + ah.distanceFromOrigin());
     System.out.println("midpoint");
   }
