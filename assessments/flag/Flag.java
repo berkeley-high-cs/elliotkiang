a0a1a7b7fa0585b2b53f64821d35456addf477fc
elliotkiang <142453988+elliotkiang@users.noreply.github.com>
2023-12-04 22:31:38 -0800

    Updating assessments/flag/Flag.java

--- assessments/flag/Flag.java
+++ assessments/flag/Flag.java
@@ -1,5 +1,4 @@
 import com.gigamonkeys.bhs.ImageGenerator;
-
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Polygon;
@@ -24,30 +23,45 @@ public class Flag implements ImageGenerator {
     // next line but to get you started, here's an example of calling the
     // provided drawStar method to draw a star, in this case a red star with a
     // diameter of 100 at the center of the canvas.
-    int hoist = (int)(height*0.9);
-    int stripeHeight = (int)(hoist/13);
-    int fly = (int)(hoist*1.9);
-    int topLeftCorner = (width-fly)/2;
-    int centerHeight = ((hoist-13*stripeHeight)/2);
-    for(int i=0;i<13;i++){
-      if(i%2==0){
+    int hoist = (int) (height * 0.9);
+    int stripeHeight = (int) (hoist / 13);
+    int fly = (int) (hoist * 1.9);
+    int topLeftCorner = (width - fly) / 2;
+    int centerHeight = ((height - hoist) / 2);
+    for (int i = 0; i < 13; i++) {
+      if (i % 2 == 0) {
         g.setColor(RED);
-        g.fillRect(topLeftCorner, i*stripeHeight+centerHeight, fly, stripeHeight);
-      }
-      else{
+        g.fillRect(
+          topLeftCorner,
+          i * stripeHeight + centerHeight,
+          fly,
+          stripeHeight
+        );
+      } else {
         g.setColor(WHITE);
-        g.fillRect(topLeftCorner, i*stripeHeight+centerHeight, fly, stripeHeight);
+        g.fillRect(
+          topLeftCorner,
+          i * stripeHeight + centerHeight,
+          fly,
+          stripeHeight
+        );
       }
     }
     g.setColor(BLUE);
-    int unionWidth = (int)(fly*0.4);
-    int starDiameter = (int)(stripeHeight*0.8);
-    g.fillRect(topLeftCorner,0+centerHeight,unionWidth,7*stripeHeight);
-    for(int i=1;i<=9;i++){
-      if(i%2==0){
-        for(int o=0;o<12;o++){
-          if(o%2 != 0 && o !=0){
-          drawStar(g, WHITE, topLeftCorner+o*((unionWidth)/12), topLeftCorner,starDiameter);
+    int unionWidth = (int) (fly * 0.4);
+    int starDiameter = (int) (stripeHeight * 0.8);
+    g.fillRect(topLeftCorner, 0 + centerHeight, unionWidth, 7 * stripeHeight);
+    for (int i = 1; i <= 9; i++) {
+      if (i % 2 == 0) {
+        for (int o = 0; o < 12; o++) {
+          if (o % 2 != 0 && o != 0) {
+            drawStar(
+              g,
+              WHITE,
+              topLeftCorner + o * ((unionWidth) / 12),
+              topLeftCorner,
+              starDiameter
+            );
           }
         }
       }
@@ -111,5 +125,4 @@ public class Flag implements ImageGenerator {
   private void addPointOnCircle(Polygon p, int x, int y, int r, double a) {
     p.addPoint(x + (int) (Math.cos(a) * r), y + (int) (Math.sin(a) * r));
   }
-
 }
