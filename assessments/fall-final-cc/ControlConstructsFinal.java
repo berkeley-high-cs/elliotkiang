4c9018b81112e5e553d9353b40039461bc0fae34
elliotkiang <142453988+elliotkiang@users.noreply.github.com>
2023-12-22 12:11:12 -0800

    Updating assessments/fall-final-cc/ControlConstructsFinal.java

--- assessments/fall-final-cc/ControlConstructsFinal.java
+++ assessments/fall-final-cc/ControlConstructsFinal.java
@@ -17,7 +17,7 @@ public class ControlConstructsFinal {
       return "F";
     }
   }
-  public countOddNeighbors(int[] nums){
+  public int countOddNeighbors(int[] nums){
     int count=0;
     for(int i=0;i<nums.length-1;i++){
       if(nums[i]%2==1&&nums[i+1]%2==1){
