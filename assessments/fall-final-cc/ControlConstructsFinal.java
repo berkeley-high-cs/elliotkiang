54a361a01fbf0482445bc326ed704b63ba989921
elliotkiang <142453988+elliotkiang@users.noreply.github.com>
2023-12-22 12:17:59 -0800

    Updating assessments/fall-final-cc/ControlConstructsFinal.java

--- assessments/fall-final-cc/ControlConstructsFinal.java
+++ assessments/fall-final-cc/ControlConstructsFinal.java
@@ -35,6 +35,14 @@ public class ControlConstructsFinal {
     }
     return oddTotal;
   }
+  public int maxValue(int[] nums){
+    int max=0;
+    for(int i=0;i<nums.length;i++){
+      if(nums[i]>max){
+        max=nums[i];
+      }
+      return max;
+  }
   // Your code goes here
 
 }
