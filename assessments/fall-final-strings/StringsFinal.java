d4b98e4ec56559b3c4c262ec14e14475959d9ab9
elliotkiang <142453988+elliotkiang@users.noreply.github.com>
2023-12-22 10:50:13 -0800

    Updating assessments/fall-final-strings/StringsFinal.java

--- assessments/fall-final-strings/StringsFinal.java
+++ assessments/fall-final-strings/StringsFinal.java
@@ -1,23 +1,31 @@
 public class StringsFinal {
-public boolean isEmpty(String word){
-  return(word.length()==0);
-}
-public String nameOfCity(){
-  return "Berkeley";
-}
-public String shout(String word){
-  return(word.toUpperCase());
-}
-public String whisper(String word){
-  return(word.toLowerCase());
-}
-public String longer(String former, String latter){
-  if(former.length()>=latter.length()){
-    return former;
-  } else{ 
-    return latter;
+
+  public boolean isEmpty(String word) {
+    return (word.length() == 0);
   }
-}
+
+  public String nameOfCity() {
+    return "Berkeley";
+  }
+
+  public String shout(String word) {
+    return (word.toUpperCase());
+  }
+
+  public String whisper(String word) {
+    return (word.toLowerCase());
+  }
+
+  public String longer(String former, String latter) {
+    if (former.length() >= latter.length()) {
+      return former;
+    } else {
+      return latter;
+    }
+  }
+  public boolean isAtLeastNCharactersLong (String word, int n){
+    return(word.length()>=n);
+  }
   // Your code goes here
 
 }
