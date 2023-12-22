de1c507e6f1afa6d21afdee0535f564b990939ef
elliotkiang <142453988+elliotkiang@users.noreply.github.com>
2023-12-22 11:52:02 -0800

    Updating assessments/fall-final-arrays/ArraysFinal.java

--- assessments/fall-final-arrays/ArraysFinal.java
+++ assessments/fall-final-arrays/ArraysFinal.java
@@ -32,7 +32,7 @@ public class ArraysFinal {
     return backwards == nums;
   }
   public int[] squares(int[] nums){
-    for(int i = 0; i<=nums.length;i++){
+    for(int i = 0; i<nums.length;i++){
       nums[i]=nums[i]*nums[i];
     }
     return nums;
