017e0a6b16648fb18fdc50c7f544a2c769695845
elliotkiang <142453988+elliotkiang@users.noreply.github.com>
2023-12-22 12:05:40 -0800

    Updating assessments/fall-final-cc/ControlConstructsFinal.java

--- assessments/fall-final-cc/ControlConstructsFinal.java
+++ assessments/fall-final-cc/ControlConstructsFinal.java
@@ -1,5 +1,9 @@
 public class ControlConstructsFinal {
-
+  public void fill(int[] nums, int replacement){
+    for(int i=0;i<nums.length;i++){
+      nums[i]=replacement;
+    }
+  }
   // Your code goes here
 
 }
