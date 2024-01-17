1213c7193bfe2f63c3fb47a14af84a6f82806c13
elliotkiang <142453988+elliotkiang@users.noreply.github.com>
2024-01-17 14:51:49 -0800

    Updating assignments/crossword/Crossword.java

--- assignments/crossword/Crossword.java
+++ assignments/crossword/Crossword.java
@@ -8,7 +8,7 @@ public class Crossword {
     int n = s2.indexOf("-");
     for(int i=0,i<s2.length();i++){
       if(s2.indexOf("-")==-1){
-        s2=s2.substring(0,s2.indexOf("-"))
+        s2=s2.substring(0,s2.indexOf("-"));
       }
     }
     return s2==s1;
