85ae3fc4819579df37de8e14f26e9fbbccd0cf05
elliotkiang <142453988+elliotkiang@users.noreply.github.com>
2024-01-17 15:09:50 -0800

    Updating assignments/crossword/Crossword.java

--- assignments/crossword/Crossword.java
+++ assignments/crossword/Crossword.java
@@ -1,20 +1,14 @@
 public class Crossword {
 
   public boolean fits(String guess, String blank) {
-    if (guess.length() == blank.length() && blank.indexOf("-") == -1) {
-      return guess == blank;
-    } else if (blank.length() != guess.length()) {
-      return false;
-    } else {
-      for (int i = 0; i < blank.length(); i++) {
-        blank =
-          blank.substring(0, blank.indexOf("-")) +
-          blank.substring(blank.indexOf("-") + 1);
-        guess =
-          guess.substring(0, blank.indexOf("-")) +
-          guess.substring(blank.indexOf("-") + 1);
+    if(guess.length()==blank.length()){
+      for(int i=0;i<blank.length){
+        if(guess.substring(0,i) != blank.substring(0,i) && blank.substring(0,i) != "-"){
+          return false;
+        }
       }
-      return guess==blank;
+      return true;
     }
+    return false;
   }
 }
