b909792103ab99c150d38d04673bb50a7d2dbfb7
elliotkiang <142453988+elliotkiang@users.noreply.github.com>
2024-01-26 14:48:04 -0800

    Creating assignments/primes-2/Primes.java

---
+++ assignments/primes-2/Primes.java
@@ -0,0 +1,21 @@
+/*
+ * Recall tha a number is prime if it is not divisible by any number other than
+ * 1 and itself. 1 is not prime. Thus you can test whether a number greater than
+ * 1 is prime by checking whether it is divisible by any smaller number greater
+ * than one.
+ *
+ * Remember that you only need to check possible factors up to (but including)
+ * the square root of the number since any factor larger than the square root
+ * would have to be multiplied by some factor smaller than the square root to
+ * get the number.
+ *
+ * Also note that you only really need to check whether the number is divisible
+ * by the prime numbers less than or equal to the square root because if a
+ * number is divisible by a composite number it is divisible by all of that
+ * composite number's prime factors.
+ */
+import java.util.ArrayList;
+
+public class Primes {
+
+}
