415ad48872c05c4bf20840b2c1fb6e5179dba3b1
elliotkiang <142453988+elliotkiang@users.noreply.github.com>
2024-01-26 15:06:11 -0800

    Updating assignments/primes-2/Primes.java

--- assignments/primes-2/Primes.java
+++ assignments/primes-2/Primes.java
@@ -30,7 +30,7 @@ public class Primes {
     if(n == 1){
       return false;
     }
-    for(int i=1;i<Math.sqrt(n);i++){
+    for(int i=2;i<Math.sqrt(n);i++){
       if(n % i == 0){
       return false;
       }
