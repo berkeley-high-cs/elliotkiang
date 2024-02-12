7c3e6191acbd7a52d30a9c21f0e94f0be0b48e78
elliotkiang <142453988+elliotkiang@users.noreply.github.com>
2024-02-12 15:01:28 -0800

    Updating assignments/counting/Counting.java

--- assignments/counting/Counting.java
+++ assignments/counting/Counting.java
@@ -6,5 +6,13 @@ public class Counting {
   public boolean isVowel(String c) {
     return "aeiou".indexOf(c) != -1;
   }
-
+  public int countLetter(String word, String letter){
+    int count = 0;
+    for(int i = 0, i < word.length(); i++){
+      if(word.substring(i,i+1).equals(letter)){
+        count++;
+      }
+    }
+    return count;
+  }
 }
