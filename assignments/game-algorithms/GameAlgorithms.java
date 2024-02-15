3d903f0a4e678100c88502b87234b9d69857c309
elliotkiang <142453988+elliotkiang@users.noreply.github.com>
2024-02-15 14:46:54 -0800

    Updating assignments/game-algorithms/GameAlgorithms.java

--- assignments/game-algorithms/GameAlgorithms.java
+++ assignments/game-algorithms/GameAlgorithms.java
@@ -1,11 +1,11 @@
 public class GameAlgorithms {
   public boolean inBounds(int[][] list, int row, int col){
-    if(row >= list.length || row >= 0){
-      return false;
-    } else if(col >= list[row].length || col >= 0){
-      return false;
-    } else {
+    if(row <= list.length || row >= 0){
       return true;
+    } else if(col <= list[row].length || col >= 0){
+      return true;
+    } else {
+      return false;
     }
   }
 
