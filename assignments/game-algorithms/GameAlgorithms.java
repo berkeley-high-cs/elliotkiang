f5911b11fc3fb3c850a184302b1e7669c7adb86d
elliotkiang <142453988+elliotkiang@users.noreply.github.com>
2024-02-15 14:57:37 -0800

    Updating assignments/game-algorithms/GameAlgorithms.java

--- assignments/game-algorithms/GameAlgorithms.java
+++ assignments/game-algorithms/GameAlgorithms.java
@@ -7,7 +7,7 @@ public class GameAlgorithms {
   }
   public int sum8Neighbors(int[][] array, int row, int col){
     int sum = 0;
-    if(inBouds(array, row, col)){
+    if(inBounds(array, row, col)){
       for(int i = 1 ; i < -2 ; i--){
         for(int o = -1 ; o < 2 ; o++){
           if(inBounds(array,row + i, col + o)){
