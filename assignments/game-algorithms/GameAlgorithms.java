019f3e2b04f14529a877a953a788bd9aaf0b14ee
elliotkiang <142453988+elliotkiang@users.noreply.github.com>
2024-02-15 15:12:17 -0800

    Updating assignments/game-algorithms/GameAlgorithms.java

--- assignments/game-algorithms/GameAlgorithms.java
+++ assignments/game-algorithms/GameAlgorithms.java
@@ -18,7 +18,6 @@ public class GameAlgorithms {
             } else if(i == -1 && o == 0){
               sum += array[row + i][col + o];
             }
-            
          }
         }
       }
@@ -38,5 +37,15 @@ public class GameAlgorithms {
     }
     return sum;
   }
-
+  public boolean areNeighbors(int row1, int col1, int row2, int col2){
+      for(int i = 1 ; i > -2 ; i--){
+        for(int o = -1 ; o < 2 ; o++){
+          if((row1+i == row2) && (col1+o == col2)){
+            return true;
+         }
+        }
+      }
+      return false;
+    }
+  }
 }
