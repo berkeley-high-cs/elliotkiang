f593e612afe8ec4d7a31dfa0db75a931c8e839f3
elliotkiang <142453988+elliotkiang@users.noreply.github.com>
2024-02-15 15:01:41 -0800

    Updating assignments/game-algorithms/GameAlgorithms.java

--- assignments/game-algorithms/GameAlgorithms.java
+++ assignments/game-algorithms/GameAlgorithms.java
@@ -9,12 +9,9 @@ public class GameAlgorithms {
     int sum = 0;
     if(inBounds(array, row, col)){
       for(int i = 1 ; i > -2 ; i--){
-        System.out.println("0");
         for(int o = -1 ; o < 2 ; o++){
-          System.out.println("1");
           if(inBounds(array,row + i, col + o)){
-            sum += array[row + i][col + o];
-          System.out.println("2");
+            sum += 1; //array[row + i][col + o];
          }
         }
       }
