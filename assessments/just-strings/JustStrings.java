3593ce6b1b4381e131e8752a34019626d8c47088
elliotkiang <142453988+elliotkiang@users.noreply.github.com>
2024-02-21 14:40:05 -0800

    Updating assessments/just-strings/JustStrings.java

--- assessments/just-strings/JustStrings.java
+++ assessments/just-strings/JustStrings.java
@@ -6,8 +6,7 @@ public class JustStrings {
     return "aeiou".indexOf(c) != -1;
   }
   public String insert(String base, String add, int pos){
-    base = base.substring(0,pos) + add + base.substring(pos);
-    return base;
+    return base.substring(0,pos) + add + base.substring(pos);;
   }
   public String deleteFirst(String base, String remove){
     return base;
