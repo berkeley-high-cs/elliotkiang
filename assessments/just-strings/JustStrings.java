f1ed5af7cf5917579531bcd6f587a1fb161a5537
elliotkiang <142453988+elliotkiang@users.noreply.github.com>
2024-02-21 14:48:36 -0800

    Updating assessments/just-strings/JustStrings.java

--- assessments/just-strings/JustStrings.java
+++ assessments/just-strings/JustStrings.java
@@ -5,25 +5,37 @@ public class JustStrings {
   public boolean isVowel(String c) {
     return "aeiou".indexOf(c) != -1;
   }
-  public String insert(String base, String add, int pos){
-    return base.substring(0,pos) + add + base.substring(pos);
+
+  public String insert(String base, String add, int pos) {
+    return base.substring(0, pos) + add + base.substring(pos);
   }
-  public String deleteFirst(String base, String remove){
-    return base.substring(0,base.indexOf(remove)) + base.substring(base.indexOf(remove)+remove.length());
+
+  public String deleteFirst(String base, String remove) {
+    return (
+      base.substring(0, base.indexOf(remove)) +
+      base.substring(base.indexOf(remove) + remove.length())
+    );
   }
-  public String deleteAll(String base, String remove){
-    while(base.indexOf(remove) != -1){
-      base = base.substring(0,base.indexOf(remove)) + base.substring(base.indexOf(remove)+remove.length());
+
+  public String deleteAll(String base, String remove) {
+    while (base.indexOf(remove) != -1) {
+      base =
+        base.substring(0, base.indexOf(remove)) +
+        base.substring(base.indexOf(remove) + remove.length());
     }
     return base;
   }
-  public String nCharacters(String base, int n, int i){
-    return base.substring(i,i+n);
+
+  public String nCharacters(String base, int n, int i) {
+    return base.substring(i, i + n);
   }
-  public String nCharactersBefore(String base, int n, String other){
-    return base.substring(base.indexOf(other)-n,base.indexOf(other));
+
+  public String nCharactersBefore(String base, int n, String other) {
+    return base.substring(base.indexOf(other) - n, base.indexOf(other));
   }
-  public String nCharactersAfter(String base, int n, String other){
-    return base.substring(base.indexOf(other),base.indexOf(other)+n);
+
+  public String nCharactersAfter(String base, int n, String other) {
+    int firstPos = base.indexOf(other) + other.length();
+    return base.substring(firstPos, firstPos + n);
   }
 }
