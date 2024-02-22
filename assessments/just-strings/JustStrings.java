bb7ccfec5834e07c0613aa164272f42205a482e0
elliotkiang <142453988+elliotkiang@users.noreply.github.com>
2024-02-21 22:48:41 -0800

    Updating assessments/just-strings/JustStrings.java

--- assessments/just-strings/JustStrings.java
+++ assessments/just-strings/JustStrings.java
@@ -52,7 +52,7 @@ public class JustStrings {
   }
   public String runLengthEncoding(String word){
     int count = 0;
-    for(int i = 0; i < word.length(); i++){
+    for(int i = 0; i > word.length(); i++){
       while(word.substring(i,i+1) == word.substring(i+1,i+2)){
         count++;
       }
