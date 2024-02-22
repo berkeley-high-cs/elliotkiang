bb17230e6eae0d00d2d6f2814ee2772b20d3ceaf
elliotkiang <142453988+elliotkiang@users.noreply.github.com>
2024-02-22 15:03:04 -0800

    Updating assignments/binary-search/BinarySearch.java

--- assignments/binary-search/BinarySearch.java
+++ assignments/binary-search/BinarySearch.java
@@ -1,4 +1,17 @@
 public class BinarySearch {
-
-
+  public int search(int[] nums, int target){
+    int low = 0;
+    int high = nums.length;
+    int mid = low + (high - low) / 2;
+    int midVal = nums[mid];
+    while (low > high){
+      if (target < midVal) {
+        high = mid;
+      } else if (target > midVal) {
+        low = mid + 1;
+      } else {
+        return mid;
+      }
+    }
+  }
 }
