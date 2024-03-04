5735e1dd3e5b9e76917c7f21f118140d2975fd1d
elliotkiang <142453988+elliotkiang@users.noreply.github.com>
2024-03-04 14:57:47 -0800

    Updating assignments/area/Shapes.java

--- assignments/area/Shapes.java
+++ assignments/area/Shapes.java
@@ -1,3 +1,12 @@
 // Write your classes below this class but leave off the public access
 // modifier. (This is because this code will get compiled as a single file and
 // there can only be one public class per file.)
+class Circle{
+  Circle soapFat = new Shape
+  String color = null;
+  double radius = 0;
+  Circle(String color, double radius){
+    this.color = color;
+    this.radius = radius;
+  }
+}
