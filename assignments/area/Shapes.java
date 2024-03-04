2e46249f4297794c4500ebe3f42122d58e24eea7
elliotkiang <142453988+elliotkiang@users.noreply.github.com>
2024-03-04 14:53:23 -0800

    Creating assignments/area/Shapes.java

---
+++ assignments/area/Shapes.java
@@ -0,0 +1,3 @@
+// Write your classes below this class but leave off the public access
+// modifier. (This is because this code will get compiled as a single file and
+// there can only be one public class per file.)
