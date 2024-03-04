c634c7ae62ef1bf88fe4a0582348551641a7b96e
elliotkiang <142453988+elliotkiang@users.noreply.github.com>
2024-03-04 14:59:07 -0800

    Updating assignments/area/Shapes.java

--- assignments/area/Shapes.java
+++ assignments/area/Shapes.java
@@ -2,7 +2,6 @@
 // modifier. (This is because this code will get compiled as a single file and
 // there can only be one public class per file.)
 class Circle{
-  Circle soapFat = new Shape
   String color = "";
   double radius = 0;
   Circle(String color, double radius){
