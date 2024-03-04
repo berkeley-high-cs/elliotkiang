c99be3edf72eac15dd6dbd09f834774142af3cc9
elliotkiang <142453988+elliotkiang@users.noreply.github.com>
2024-03-04 15:29:30 -0800

    Updating assignments/leap-year/LeapYear.java

--- assignments/leap-year/LeapYear.java
+++ assignments/leap-year/LeapYear.java
@@ -2,7 +2,7 @@ public class LeapYear {
   public boolean isLeapYear(int year){
     return year % 4 == 0 && year % 100 != 0;
   }
-  public numberOfLeapYears(int firstYear, int secondYear){
+  public int numberOfLeapYears(int firstYear, int secondYear){
     return (secondYear - firstYear)/4;
   }
 
