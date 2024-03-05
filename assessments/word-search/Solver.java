71f0314ed2695238ba607139160b0bbfd877febd
elliotkiang <142453988+elliotkiang@users.noreply.github.com>
2024-03-05 14:41:50 -0800

    Updating assessments/word-search/Solver.java

--- assessments/word-search/Solver.java
+++ assessments/word-search/Solver.java
@@ -2,6 +2,6 @@ import java.util.*;
 
 public class Solver {
   public boolean inBounds(int row, int col, String[][] array){
-    return row < array.size() && col < array[row].size;
+    return row < array.length && col < array[row].length;
   }
 }
