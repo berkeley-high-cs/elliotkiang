5ff027ad86667c4799faa3abad2024d943baac62
elliotkiang <142453988+elliotkiang@users.noreply.github.com>
2024-03-05 15:19:39 -0800

    Updating assessments/word-search/Solver.java

--- assessments/word-search/Solver.java
+++ assessments/word-search/Solver.java
@@ -10,13 +10,14 @@ public class Solver {
 
   public boolean inGrid(String word, String[][] array) {
     String targetLetter = word.substring(0, 1);
+    int[] coordinates = [2];
     for (int i = 0; i < word.length(); i++) {
       for (int r = 1; r > -2; r--) {
         for (int c = -1; c < 2; c++) {
           if (
-            inBounds(i + r, i + c, array) &&
-            array[i + r][i + c].equals(targetLetter) &&
-            (r != 0 && c != 0)
+            inBounds(coordinates[0] + r, coordinates[1] + c, array) &&
+            array[coordinates[0] + r][coordinates[1] + c].equals(targetLetter) &&
+            !(r == 0 && c == 0)
           ) {
             return true;
           }
