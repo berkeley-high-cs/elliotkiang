f73ae23559b3c84b946f18f6a2dd454c7a6c1492
elliotkiang <142453988+elliotkiang@users.noreply.github.com>
2024-03-06 14:49:35 -0800

    Updating assessments/word-search/Solver.java

--- assessments/word-search/Solver.java
+++ assessments/word-search/Solver.java
@@ -8,23 +8,31 @@ public class Solver {
     );
   }
 
-
-  public int[] checkAdjacent(int row, int col, String letter, String[][] array){
-    int[] pos = new int[2];
-    for (int r = 1; r > -2; r--) {
+  public boolean inGrid(String word, String[][] array) {
+    String letters = "";
+    String targetLetter = word.substring(0, 1);
+    int coordinateRow = 0;
+    int coordinateCol = 0;
+    for (int i = 0; i < word.length(); i++) {
+      for (int r = 1; r > -2; r--) {
         for (int c = -1; c < 2; c++) {
           if (
-            inBounds(row + r, col + c, array) &&
-            array[row + r][col + c].equals(letter) &&
+            inBounds(coordinateRow + r, coordinateCol + c, array) &&
+            array[coordinateRow + r][coordinateCol + c].equals(targetLetter) &&
             !(r == 0 && c == 0)
           ) {
-            pos[1] = r;
-            pos[2] = c;
-            return pos;
+            coordinateRow = r;
+            coordinateCol = c;
+            letters = letters + targetLetter;
           }
         }
       }
-      pos[1] = -1;
-      return pos;
+      if(i+2 >= word.length()){
+      targetLetter = word.substring(i+1, i+2);
+      } else {
+      targetLetter = word.substring(i+1, i+2);
+      }
+    }
+    return letters.equals(word);
   }
 }
