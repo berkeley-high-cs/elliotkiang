f5cc2a9f9d6abb1b51400a214a5d360c175cd927
elliotkiang <142453988+elliotkiang@users.noreply.github.com>
2024-03-07 14:50:15 -0800

    Updating assessments/word-search-3/Solver.java

--- assessments/word-search-3/Solver.java
+++ assessments/word-search-3/Solver.java
@@ -2,8 +2,6 @@ import java.util.*;
 
 public class Solver extends Helper3 {
 
-  public class Solver extends Helper2 {
-
     public boolean inBounds(int row, int col, String[][] array) {
       return (
         (row < array.length && row >= 0) &&
