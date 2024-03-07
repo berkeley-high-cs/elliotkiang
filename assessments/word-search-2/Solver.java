a04f8f38229260c811aea893173085fccc291823
elliotkiang <142453988+elliotkiang@users.noreply.github.com>
2024-03-07 14:45:01 -0800

    Updating assessments/word-search-2/Solver.java

--- assessments/word-search-2/Solver.java
+++ assessments/word-search-2/Solver.java
@@ -6,4 +6,14 @@ public class Solver extends Helper2 {
       (row < array.length && row >= 0) && (col < array[row].length && col >= 0)
     );
   }
+  public boolean inGrid(String word, String puzzle){
+    for(int r = 0; i < puzzle.length; i++){
+      for(int c = 0; c < puzzle[r].length; i++){
+        if(startingAt(word, puzzle, r, c)){
+          return true;
+        }
+      }
+    }
+    return false;
+  }
 }
