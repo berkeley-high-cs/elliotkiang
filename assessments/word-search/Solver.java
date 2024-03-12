7bb504ae12cacc6fcbe894bad9e7923cf9818982
elliotkiang <142453988+elliotkiang@users.noreply.github.com>
2024-03-12 15:17:29 -0700

    Updating assessments/word-search/Solver.java

--- assessments/word-search/Solver.java
+++ assessments/word-search/Solver.java
@@ -54,6 +54,11 @@ public class Solver {
   }
   public ArrayList<String> found(String[][] grid, ArrayList<String> words){
     ArrayList<String> wordsFound = new ArrayList<>();
+    for(int i = 0; i < words.size(); i++){
+      if(inGrid(words.get(i))){
+        wordsFound.add(words.get(i));
+      }
+    }
     return wordsFound;
   }
 }
