7108d23cfe951d0a2b48d6c16fb7ecd6589760b0
elliotkiang <142453988+elliotkiang@users.noreply.github.com>
2024-03-14 14:57:03 -0700

    Updating assessments/strings-3/Strings3.java

--- assessments/strings-3/Strings3.java
+++ assessments/strings-3/Strings3.java
@@ -9,6 +9,7 @@ public class Strings3 {
       base.substring(base.indexOf(marker))
     );
   }
+
   public String insertAfter(String base, String insert, String marker) {
     return (
       base.substring(0, base.indexOf(marker) + marker.length()) +
@@ -16,29 +17,41 @@ public class Strings3 {
       base.substring(base.indexOf(marker) + marker.length())
     );
   }
-  public String reverse(String word){
+
+  public String reverse(String word) {
     String newWord = "";
-    for(int i = word.length() - 1; i < -1; i--){
-      newWord = word.substring(i,i+1) + newWord;
+    for (int i = word.length() - 1; i < -1; i--) {
+      newWord = word.substring(i, i + 1) + newWord;
     }
     return newWord;
   }
-  public String despace(String word){
-    while(word.indexOf(" ") != -1){
-      word = word.substring(0,word.indexOf(" ")) + word.substring(word.indexOf(" ") + 1);
+
+  public String despace(String word) {
+    while (word.indexOf(" ") != -1) {
+      word =
+        word.substring(0, word.indexOf(" ")) +
+        word.substring(word.indexOf(" ") + 1);
     }
     return word;
   }
-  public String join(ArrayList<String> words, String delimiter){
+
+  public String join(ArrayList<String> words, String delimiter) {
     String newWord = "";
-    for(int i = 0; i < words.size(); i++){
-      if(i != 0){
+    for (int i = 0; i < words.size(); i++) {
+      if (i != 0) {
         newWord = newWord + delimiter + words.get(i);
       } else {
         newWord = words.get(i);
       }
-      
     }
     return newWord;
+  }
+  public boolean isSorted(array[Strings] words){
+    for (int i = 0; i < words.length; i++) {
+      if (words[i].compareTo(words[i+1]) < 1) {
+        return false
+      }
+    }
+    return true;
   }
 }
