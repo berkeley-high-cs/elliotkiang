e762015383b9bb9be745568478c5d388f259a8cf
elliotkiang <142453988+elliotkiang@users.noreply.github.com>
2024-03-14 14:42:19 -0700

    Updating assessments/strings-3/Strings3.java

--- assessments/strings-3/Strings3.java
+++ assessments/strings-3/Strings3.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
 
 public class Strings3 {
-
+  public String insertBefore(String base, String insert, String marker){
+    return(base.substring(0,base.indexOf(marker)) + insert + base.substring(base.indexOf(marker)))
+  }
 }
