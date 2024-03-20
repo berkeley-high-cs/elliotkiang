373aa890a8ac92ba730928943eca6f532eff067d
elliotkiang <142453988+elliotkiang@users.noreply.github.com>
2024-03-20 15:14:42 -0700

    Updating assignments/string-recursion/StringRecursion.java

--- assignments/string-recursion/StringRecursion.java
+++ assignments/string-recursion/StringRecursion.java
@@ -5,4 +5,14 @@ public class StringRecursion {
     }
       return 1 + length(word.substring(1));
   }
+  public int countXs(String word){
+    if(word.isEmpty()){
+      return 0;
+    } else {
+      if(word.substring(0,1).equals("x")){
+        return countXs(word.substring(1)) + 1;
+      }
+    }
+      
+  }
 }
