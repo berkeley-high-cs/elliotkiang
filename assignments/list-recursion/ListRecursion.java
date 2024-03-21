2f2e2bc9f34149ba2adda4c9b467b4e801adca8d
elliotkiang <142453988+elliotkiang@users.noreply.github.com>
2024-03-21 14:42:22 -0700

    Updating assignments/list-recursion/ListRecursion.java

--- assignments/list-recursion/ListRecursion.java
+++ assignments/list-recursion/ListRecursion.java
@@ -1,6 +1,14 @@
 import java.util.*;
 
 public class ListRecursion {
+  public int sum(ArrayList<Integer> list){
+    int count = 0;
+    if(rest(list[]).size() == 0){
+      return 0;
+    }
+    count += list.get(0);
+    return sum(list(1));
+  }
 
   /*
    * A useful helper method. Returns a list containing all but the first element
